67f35e9b7eaa643b073fa76f359ab08e500326cb
Kerim-gn <kerim.gnn@hotmail.com>
2025-02-18 00:04:54 +0100

    loop_rechnung

--- index.js
+++ index.js
@@ -1,19 +1,92 @@
 
+var data = [
+    {
+        year: 2010,
+        revenue: [
+            {
+                month: "Q1",
+                revenue: 21500
+            },
+            {
+                month: "Q2",
+                revenue: 27300
+            },            {
+                month: "Q3",
+                revenue: 24800
+            },            {
+                month: "Q4",
+                revenue: 28900
+            },
+        ]
+    },
+    {
+        year: 2011,
+        revenue: [
+            {
+                month: "Q1",
+                revenue: 26400
+            },
+            {
+                month: "Q2",
+                revenue: 30300
+            },            {
+                month: "Q3",
+                revenue: 26200
+            },            {
+                month: "Q4",
+                revenue: 29100
+            },
+        ]
+    },   
+    {
+        year: 2012,
+        revenue: [
+            {
+                month: "Q1",
+                revenue: 31200
+            },
+            {
+                month: "Q2",
+                revenue: 29400
+            },            {
+                month: "Q3",
+                revenue: 34600
+            },            {
+                month: "Q4",
+                revenue: 37200
+            },
+        ]
+    }
+]
+var totalRevenue = 0;
+
 
 function main() {
-    var inputspeed = document.getElementById("speed");
-    var inputmaxspeed = document.getElementById("maxspeed");
-    var blitz = document.getElementById("Blitzer")
-
-    var speed = parseInt(inputspeed.value); 
-    var check = blitz.checked;
-    var maxspeed = parseInt(inputmaxspeed.valu);
-    var endspeed = maxspeed * 1.10; //  = maxspeed +((maxspeed/100)*10); ist die lösung bei mir ist das probelm das 55 zu schnell nicht durchgeht 
     
-    if(speed >= endspeed && check == true){
-        alert("der blitzer ist durchgegangen")
-    }else {
-       alert("verfahren wurde eingestellt")
-    }
 
-}
+     for(let i = 0; i < data.length; i++){
+        let yearindata = data[i]
+            console.log("year: " + yearindata.year );  
+            for(let j = 0; j < yearindata.revenue.length; j++){
+                let newrevenue = yearindata.revenue[j]
+                console.log("die aktuelle quatal zahl: " + newrevenue.month);
+
+                totalRevenue += newrevenue.revenue
+                console.log(" ausgabe im quatalmonat: " + newrevenue.revenue)
+                
+                }
+            }
+            console.log("gesamt ausgabe " + totalRevenue)
+            
+     }
+   
+    
+
+          
+           
+    
+        
+        // ↑
+    
+    
+    // Sample Data
