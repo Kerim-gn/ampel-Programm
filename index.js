a98147497b33d439543e68003786ffb4ec6c4108
Kerim-gn <kerim.gnn@hotmail.com>
2025-03-13 13:39:38 +0100

    infosFürMich

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
+ /*
+ var = kann auch verwendet werden wenn die variable ausßerhalb von dem block steht (BLOCk = {})
+ let = kann nur in dem jeweiligen blcok ausgelesen werden verschwindet danach {hallo} console.log(hallo kann nicht gefunden werden)
+ const = erstellen von klassen und erstellung von variablen die nicht verändert werden könnenn 
+ objekte = z.b const auto = {name : ... , alter : .... oder auch array in array (nachschauen)}
  
+ */
 
- 
- 
- function celsiusToFahrenheit(x){
-        result = (x * 9.0 / 5.0) + 32
-    return result;
 
- }
- function main () {
-    Abfrage  = prompt()
-    ergebniss = celsiusToFahrenheit(Abfrage)
-    console.log(result);
 
- }
+
