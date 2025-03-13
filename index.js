29ea8908a338eee60fcd0811b8b7c35a10bfa537
Kerim-gn <kerim.gnn@hotmail.com>
2025-03-13 14:07:00 +0100

    ProbeDurchführung

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
- /*
- var = kann auch verwendet werden wenn die variable ausßerhalb von dem block steht (BLOCk = {})
- let = kann nur in dem jeweiligen blcok ausgelesen werden verschwindet danach {hallo} console.log(hallo kann nicht gefunden werden)
- const = erstellen von klassen und erstellung von variablen die nicht verändert werden könnenn 
- objekte = z.b const auto = {name : ... , alter : .... oder auch array in array (nachschauen)}
- 
- */
+function main (){
+   console.log("wird aufgerufen wenn ich auf den botten drücke")
+}
 
-
-
-
+function Test() {
+   console.log("wird aufgerufen wenn ich das neu gerlenrte anwende")
+}
+Test()
